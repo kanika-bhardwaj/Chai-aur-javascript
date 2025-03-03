c4176c36af65ec5fdca2662761843518defcbb14
kanika-bhardwaj <90175293+kanika-bhardwaj@users.noreply.github.com>
2025-03-03 11:39:18 +0000

    conversions

---
+++ basics/convOper.js
@@ -0,0 +1,12 @@
+let score="22"
+// console.log(typeof score);
+let valueinNumber=Number(score)
+//"33"->33
+// "33abs"->NaN
+// true->1
+//false->0
+
+// console.log(typeof valueinNumber)
+let isLoggedin="Kanika";//if its empty it will return false else true
+let booleanisLoggedin=Boolean(isLoggedin);
+console.log(booleanisLoggedin);
