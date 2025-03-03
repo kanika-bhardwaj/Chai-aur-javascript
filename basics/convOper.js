7e25bd1dc28ac97fe213ad70e177583b45dd8ff0
kanika-bhardwaj <90175293+kanika-bhardwaj@users.noreply.github.com>
2025-03-03 12:29:37 +0000

    operations done

--- basics/convOper.js
+++ basics/convOper.js
@@ -7,6 +7,31 @@ let valueinNumber=Number(score)
 //false->0
 
 // console.log(typeof valueinNumber)
-let isLoggedin="Kanika";//if its empty it will return false else true
-let booleanisLoggedin=Boolean(isLoggedin);
-console.log(booleanisLoggedin);
+// let isLoggedin="Kanika";//if its empty it will return false else true
+// let booleanisLoggedin=Boolean(isLoggedin);
+// console.log(booleanisLoggedin);
+
+// let num=33
+// let strnum=String(num);
+// console.log(strnum);
+// console.log(typeof strnum);
+
+//*************Operations*************************//
+let value=3
+let negval=-value
+// console.log(negval);
+//similarly + - / * ** %
+
+
+//not recommended
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32
+
+// console.log(+true);//still print 1
+
+// difference in ++var and var++
+// ++var increases value before current operations
+//var++ increases value for next operation
+
