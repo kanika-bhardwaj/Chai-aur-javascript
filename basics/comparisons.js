d8c5276d7af674e206b6cc32be76cb41e1a37c8a
kanika-bhardwaj <90175293+kanika-bhardwaj@users.noreply.github.com>
2025-03-03 12:36:37 +0000

    compare push

---
+++ basics/comparisons.js
@@ -0,0 +1,11 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(3<5)
+// similarly more
+
+// console.log("Kanika">1) ->unpredictable
+
+console.log("2"==2)
+console.log("2"===2)
+
+//=== and == work in a different manner "2"==2 is true but "2"===2 is false
