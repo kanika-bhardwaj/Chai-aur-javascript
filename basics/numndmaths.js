53c506fa05f04367336d5e5bfb7418e8dfadece4
kanika-bhardwaj <90175293+kanika-bhardwaj@users.noreply.github.com>
2025-03-07 13:50:12 +0000

    maths and numbers

---
+++ basics/numndmaths.js
@@ -0,0 +1,36 @@
+const marks=359
+// console.log(marks);
+const balance=new Number(379);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const other=11123.88567;
+// console.log(other.toPrecision(3));
+
+const big=100000;
+// console.log(big.toLocaleString('en-IN'));
+
+//min_value,max_value,min_safe_value,etc. also exist
+
+
+//Maths start here
+
+// console.log(Math);//try running in inspect->console to know better
+
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.7));//5
+// console.log(Math.ceil(4.2));//5
+// console.log(Math.floor(4.8));//4
+// console.log(Math.min(2,90,34,67));
+// console.log(Math.max(34,56,78));
+
+
+// console.log(Math.random());
+// console.log(Math.floor((Math.random()*10))+1);
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random() *(max-min+1))+min);//very important
+
