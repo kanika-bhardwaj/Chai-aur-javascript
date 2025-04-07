3d29e6448f0a1ded1498da40f931f6156b6f450f
kanika-bhardwaj <90175293+kanika-bhardwaj@users.noreply.github.com>
2025-04-07 09:06:51 +0000

    control flow,etc

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,58 @@
+// if(conditiontrue h){
+    // to code execute kro
+// }
+
+// const userloggedin=true;
+// if(userloggedin){
+//     console.log("he is logged in");
+// }
+// const temperatur=25;
+
+// //one or other
+// if(temperatur<50){
+//     console.log("go out");
+// }else{
+//     console.log("dont go");
+// }
+
+// //only one if true no else
+// if(2=='2'){
+//     console.log("equal");//prints
+// }
+// if(2==='2'){
+//     console.log("equal");//dont print because not strict equal due to different data types
+// }
+
+
+// const score=250;
+// if(score>100){
+//     const power="Fly";
+//     console.log(`user power:${power}`);
+// }
+
+
+// console.log(power);//gives error
+
+// const bal=1999;
+// // if(bal>500) console.log("You are amir");
+
+// if(bal<500){
+//     console.log("less than 500");
+// }else if(bal<750){
+//     console.log("less than 750");
+// }else{
+//     console.log("amiri huh");    
+// }
+
+const namethere=true;
+const debitcard=true;
+const creditcard=false;
+if(creditcard || debitcard){ //OR
+    console.log("Can buy course"); 
+}
+if(namethere && debitcard){ //AND
+    console.log("buy course");
+}else{
+    console.log("something is missing?");
+}
+
---
+++ 04_Control_flow/truthE.js
@@ -0,0 +1,22 @@
+const userEmail="kanikabhardwaj0632@gmail.com";
+if(userEmail) console.log("got user email");//if string isnt empty
+else console.log("No email?");//is string is empty
+
+// //Falsy values
+// false
+// 0
+// -0
+// BigInt 0n
+// ""
+// null
+// undefined
+// NaN
+
+// Truthy values
+//"0"
+// 'false'
+// " "
+// []
+// {}
+// function(){}
+
